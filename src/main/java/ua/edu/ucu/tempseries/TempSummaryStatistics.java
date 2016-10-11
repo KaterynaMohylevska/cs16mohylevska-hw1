35e3afa5c300470734fe3e231be346369351a425
KaterynaMohylevska <mohylevska@ucu.edu.ua>
2016-10-11 09:55:03 +0300

    Comment

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,12 +1,13 @@
 package ua.edu.ucu.tempseries;
 
 public class TempSummaryStatistics {
-    final private double avgTemp;
-    final private double devTemp;
-    final private double minTemp;
-    final private double maxTemp;
+    private final double avgTemp;
+    private final double devTemp;
+    private final double minTemp;
+    private final double maxTemp;
 
-    public TempSummaryStatistics(double avgTemp, double devTemp, double minTemp, double maxTemp) {
+    public TempSummaryStatistics(double avgTemp, double devTemp,
+                                 double minTemp, double maxTemp) {
         this.avgTemp = avgTemp;
         this.devTemp = devTemp;
         this.minTemp = minTemp;
